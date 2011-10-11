2fe1ec752440e6938329c4a973acaf4872ff6649
Valdas Sevelis <valdas.s@gmail.com>
2011-10-10 20:31:24 -0400

    Fixed script for new OpenSSL output

--- shell-script/gen_pwd.sh
+++ shell-script/gen_pwd.sh
@@ -2,6 +2,6 @@
 stty -echo
 read -p "Password: " passw; echo
 stty echo
-str=$(echo $1:$passw | openssl dgst -rmd160 | openssl enc -base64)
+str=$(echo $1:$passw | openssl dgst -rmd160 | awk '{ print $2 }' | openssl enc -a -A)
 len=${2:-"64"}
-echo $str $len |  awk '{ print substr($1,1,$2)}'
+echo $str $len | awk '{ print substr($1,1,$2)}'
